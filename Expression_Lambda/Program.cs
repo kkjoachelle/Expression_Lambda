b8a4f019d1e23b72ec824b41b761fddf2e0cfb4a
KKjoachelle <31347204+kkjoachelle@users.noreply.github.com>
2019-02-14 18:01:30 -0500

    Dépôt initial

---
+++ Expression_Lambda/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expression_Lambda
+{
+    class Program
+    {
+            delegate bool IsTeenAger(Student stud);
+        
+
+            public static void Main()
+            {
+                IsTeenAger isTeenAger = s => s.Age > 12 && s.Age < 20;
+
+                Student stud = new Student() { Age = 25 };
+
+                Console.WriteLine(isTeenAger(stud));
+
+                Console.ReadKey();
+
+            }
+    }
+
+        public class Student
+        {
+
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+}
